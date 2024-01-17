614a01f3d8c736217876d2a84c88d99c13546242
Jonathan Arana <cheoarana.dev@gmail.com>
2024-01-16 21:27:15 -0500

    Creación de primer template html con datos personales misdatos.html

--- jarana/mi_portafolio/views.py
+++ jarana/mi_portafolio/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hola mi portafolio. En el Index")
+
+def mis_datos(request):
+    return render(request,'mi_portafolio/misdatos.html',{})
