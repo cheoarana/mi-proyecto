63305a3658fd850ed228e56c5d3d58ceaf952ef1
Jonathan Arana <cheoarana.dev@gmail.com>
2024-01-24 00:04:07 -0500

    Update models
    Create models to project

--- jarana/mi_portafolio/models.py
+++ jarana/mi_portafolio/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(null=True, blank=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    address = models.TextField()
+    phone = models.CharField(max_length=13)
+    age = models.IntegerField()
+    content = models.TextField()
+    skills = models.TextField()
+
+    def __str__(self):
+        return self.first_name+" "+self.last_name
